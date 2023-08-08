8826fe889a0c3f89e00d9c0752792f68b8cb8ecf
vikash <vickymeenu023@gmail.com>
2023-08-08 17:01:55 +0530

    star pro

--- d16-20230728/practice/task3.py
+++ d16-20230728/practice/task3.py
@@ -9,7 +9,12 @@ for i in range(1,(n*n)+1):
     print((n*n)+1-i,end="")
     if i%n==0:
         print("\n")
-    
+n=int(input("enter the number :"))
+for i in range(n):
+    for j in range(n):
+    print((n*n)+1-i,end="")
+    if i%n==0:
+        print("\n")
 
     
 
