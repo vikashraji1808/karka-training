c66a056404f6bbf2525bd6fd57461983c124ce83
vikash <vickymeenu023@gmail.com>
2023-08-09 16:21:25 +0530

    task

---
+++ d16-20230728/practice/task4.py
@@ -0,0 +1,55 @@
+# a=[1,0,2,3,0,4,0,5,0]
+# # a1=[]
+# # for i in a:
+# #     if i==0:
+# #         a=0,0
+# #         # a1.append(0)
+# #         print(a1)
+# a=[]
+# for i in range(len(a)):
+#     if a[i]==0:
+#         a[i]=0,0
+#         a.append(a[i])
+# print(a)
+# given  = [1,0,2,3,0,4,0,5,0]
+# res = []
+# for i in given: 
+#     if len(res) < len(given) : 
+
+#         if i == 0 and len(res) < (len(given)-1) :
+#                 res+=[i,i]
+#         else: 
+#             res+=[i]
+    
+#     else : 
+#           break
+
+# print('a=', res)
+# split_words={}
+# str = "the quick brown fox jumps over the lazy dog the quick brown fox"
+# string=str.split()
+# for i in string:
+#     if i not in split_words:
+#         split_words[i]=1
+#     else:
+#         split_words[i]+=1
+# print (split_words)
+
+
+        
+
+   
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+        
